ba0714d481920d150fe1e3102d1982b1d58ffde3
Cris-hr <cjminisol@gmail.com>
2023-08-26 01:23:15 -0500

    componentes-estilos

--- movie-challenge/src/App.jsx
+++ movie-challenge/src/App.jsx
@@ -2,9 +2,23 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-
-function App() {
-  return <div>App</div>;
+import logo from "./assets/logotipo1.png";
+import { MoviesGrid } from "./moviesGrid";
+export function App() {
+  return (
+    <div className="homepage">
+      <header>
+        <div className="headerMovie">
+          <div className="leftHeader">
+            <img src={logo} alt={"logo"} className="loguito" />
+          </div>
+        </div>
+      </header>
+      <main>
+        <MoviesGrid />
+      </main>
+    </div>
+  );
 }
 
-export default App;
+//export default App;
---
+++ movie-challenge/src/moviesGrid.jsx
@@ -0,0 +1,13 @@
+import { MovieCard } from "./movieCard";
+import movies from "./movies.json";
+import styles from "./moviesGrid.module.css";
+
+export function MoviesGrid() {
+  return (
+    <ul className={styles.movieCardGrid}>
+      {movies.map((movie) => (
+        <MovieCard key={movie.id} movie={movie} />
+      ))}
+    </ul>
+  );
+}
