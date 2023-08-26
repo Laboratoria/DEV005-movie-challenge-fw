4af91213dcdd219095c547f1344b48cb1c20e4a7
Cris-hr <cjminisol@gmail.com>
2023-08-26 01:28:30 -0500

    edit-componentes

--- movie-challenge/src/App.jsx
+++ movie-challenge/src/App.jsx
@@ -1,6 +1,3 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import logo from "./assets/logotipo1.png";
 import { MoviesGrid } from "./moviesGrid";
